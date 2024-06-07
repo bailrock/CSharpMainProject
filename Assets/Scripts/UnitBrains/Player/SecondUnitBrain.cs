4f329ce2187f94840c65eefa1a8e4ecd9ddcc092
Shumskiy Ilya <ishumsky@ptsecurity.com>
2024-06-08 01:18:07 +0400

    add overHeat

--- Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/SecondUnitBrain.cs
@@ -15,13 +15,22 @@ namespace UnitBrains.Player
         
         protected override void GenerateProjectiles(Vector2Int forTarget, List<BaseProjectile> intoList)
         {
-            float overheatTemperature = OverheatTemperature;
-            ///////////////////////////////////////
-            // Homework 1.3 (1st block, 3rd module)
-            ///////////////////////////////////////           
-            var projectile = CreateProjectile(forTarget);
-            AddProjectileToList(projectile, intoList);
-            ///////////////////////////////////////
+            var currentTemperature = GetTemperature();
+
+            if (currentTemperature >= OverheatTemperature)
+            {
+                return;
+            }
+
+            var projectilesCount = currentTemperature + 1;
+            
+            for (var i = 0; i < projectilesCount; i++)
+            {
+                var projectile = CreateProjectile(forTarget);
+                AddProjectileToList(projectile, intoList);
+            }
+            
+            IncreaseTemperature();
         }
 
         public override Vector2Int GetNextStep()
